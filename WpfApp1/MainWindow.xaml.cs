8954e8a5811dc0865c8e20e50e0679ff82c70a1e
MagicBook <MagicBook@LAPTOP-2789C080>
2024-06-01 23:50:15 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.View;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Employee_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowEmployee wEmployee = new WindowEmployee();
+            wEmployee.Show();
+        }
+        private void Role_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowRole wRole = new WindowRole();
+            wRole.Show();
+        }
+        }
+    }
---
+++ WpfApp1/ViewModel/RoleViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    internal class RoleViewModel
+    {
+        public ObservableCollection<Role> ListRole { get; set; } = new ObservableCollection<Role>();
+ public RoleViewModel()
+        {
+            this.ListRole.Add(new Role
+            {
+                Id = 1,
+                NameRole = "Директор"
+            });
+            this.ListRole.Add(new Role
+            {
+                Id = 2,
+                NameRole = "Бухгалтер"
+            });
+            this.ListRole.Add(new Role
+            {
+                Id = 3,
+                NameRole = "Менеджер"
+            });
+        }
+
+    }
+}
